160763bf6ce712a4908150f74b88438dd08c0a1f
mintmilk <xbai67@gmail.com>
2023-12-05 22:00:10 +0800

    slurm submit

---
+++ new_gvcf.py
@@ -0,0 +1,26 @@
+import glob
+import os
+import subprocess
+
+# 获取 fastp 目录下所有样本名
+fastq_files = sorted(glob.glob("fastp/*.fq.gz"))
+SAMPLES = sorted(set(os.path.basename(f).split('.', 1)[0] for f in fastq_files))
+
+# 脚本路径
+bash_script = "qc2bam.bash"
+output_dir = "output"
+
+for sample in SAMPLES:
+    # 为每个样本构造 fq 文件路径
+    fq1 = f"fastp/{sample}.clean.R1.fq.gz"
+    fq2 = f"fastp/{sample}.clean.R2.fq.gz"
+
+    # 检查文件是否存在
+    if not os.path.exists(fq1) or not os.path.exists(fq2):
+        print(f"文件不存在: {fq1} 或 {fq2}")
+        continue
+
+    # 构造命令
+    command = f"sbatch --partition=XiaoYueHe --cpus-per-task=4 --mail-user=2392593414@qq.com --mail-type=ALL {bash_script} {fq1} {fq2} {sample} {output_dir}"
+    print(command)
+    # subprocess.run(command, shell=True) 
